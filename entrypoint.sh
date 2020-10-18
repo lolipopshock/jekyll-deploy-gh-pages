346d8b47c6f73e10fa0c11c7acca910f6203b715
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-10-18 16:55:30 -0400

    change build dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 bundle install
 bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
-bundle exec jekyll build -d ./build
+bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
-cd build
+cd _site
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages" && \
 git init && \
@@ -16,7 +16,7 @@ git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
-git commit -m'action build' > /dev/null 2>&1 && \
+git commit -m 'action build' > /dev/null 2>&1 && \
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
